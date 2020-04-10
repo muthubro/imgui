91bba3338838470917966da848395e008d79fffa
Muathasim Parambat <muth4muathasim@gmail.com>
2020-04-10 14:49:20 +0530

    Added premake file

---
+++ premake5.lua
@@ -0,0 +1,38 @@
+project "ImGui"
+	kind "StaticLib"
+	language "C++"
+	
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files {
+		"imconfig.h",
+		"imgui.h",
+		"imgui.cpp",
+		"imgui_demo.cpp",
+		"imgui_draw.cpp",
+		"imgui_widgets.cpp",
+		"imgui_internal.h",
+		"imstb_rectpack.h",
+		"imstb_textedit.h",
+        "imstb_truetype.h"
+    }
+
+	filter "system:windows"
+		systemversion "latest"
+        cppdialect "C++17"
+		staticruntime "On"
+
+    filter "system:linux"
+        pic "On"
+        systemversion "latest"
+        cppdialect "C++17"
+        staticruntime "On"
+
+    filter "configurations:Debug"
+        runtime "Debug"
+        symbols "On"
+
+    filter "configurations:Release"
+        runtime "Release"
+        optimize "On"
